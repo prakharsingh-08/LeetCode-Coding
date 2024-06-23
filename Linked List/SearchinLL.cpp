f57558fc198c1cdf07240032f51a09ad4fba21be
Prakhar Singh <125474574+prakharsingh-08@users.noreply.github.com>
2024-06-24 03:33:12 +0530

    SearchinLL.cpp

---
+++ Linked List/SearchinLL.cpp
@@ -0,0 +1,24 @@
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, struct Node* head, int key) {
+        // Code here
+        Node* curr = head;
+        while( curr->next != NULL) {
+            if( key == curr->data) {
+                return true;
+            }
+            curr = curr->next;
+        }
+        return false;
+    }
+};
