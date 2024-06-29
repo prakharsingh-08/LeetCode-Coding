8b533876ce58380cbb5c65967d592e450255f79c
Prakhar Singh <125474574+prakharsingh-08@users.noreply.github.com>
2024-06-29 22:15:49 +0530

    Group all the nodes with odd indices together followed by the nodes with even indices

---
+++ Linked List/Odd Even Linked List.cpp
@@ -0,0 +1,34 @@
+
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+
+        if(head == NULL || head -> next == NULL || head ->next->next == NULL) 
+            return head;
+
+        ListNode *odd = head;
+        ListNode *even = head -> next;
+        ListNode *even_start = head -> next;
+
+        while(odd -> next && even -> next){
+
+            odd -> next = even -> next; //CONNECT ALL ODDS
+            even -> next = odd -> next -> next; // CONNECT ALL EVENS
+            odd = odd -> next;
+            even = even -> next;
+        }
+        odd -> next = even_start;
+        return head;  
+    }
+};
