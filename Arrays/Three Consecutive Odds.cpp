ceb529cf8b55efc5a52497cae6f00005a0ab16e3
Prakhar Singh <125474574+prakharsingh-08@users.noreply.github.com>
2024-07-02 00:17:54 +0530

    Three Consecutive Odds.cpp

---
+++ Arrays/Three Consecutive Odds.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count = 0;
+
+        for(int nums : arr) {
+
+            if(nums % 2 != 0) {
+
+                count ++;
+
+                if(count == 3)
+                {
+                    return true;
+                }
+            }
+            else{
+
+                count = 0;
+            }
+        }
+
+        return false;
+    }
+};
