d39beb162ef5113945289905feb73ed8a0f63275
Prakhar Singh <125474574+prakharsingh-08@users.noreply.github.com>
2024-07-11 22:09:42 +0530

    Fibonacci Number

---
+++ Recursion/Fibonacci Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+    if( n == 0)
+        return 0;
+    if( n == 1)
+        return 1;
+    vector<int> Fibonacci(n+1);
+    Fibonacci[0] = 0;
+    Fibonacci[1] = 1;
+    for(int i = 2; i< n+1;i++)
+        Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i - 2];
+    return Fibonacci[n];    
+    }
+};
