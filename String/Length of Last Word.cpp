d2cbfd866558b3bc333e2f68a6815afb13a98c0f
Prakhar Singh <125474574+prakharsingh-08@users.noreply.github.com>
2024-07-25 22:42:13 +0530

    Length of Last Word

---
+++ String/Length of Last Word.cpp
@@ -0,0 +1,21 @@
+/*Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.*/
+
+Class Solution
+{
+  public:
+      int lenghtofLastWord(String s) {
+
+      int count = 0;
+      for(int i = s.length() - 1; i >= 0; i--){
+      if(s[i] != ' '){
+          count++;
+      }
+      else{
+          if(count > 0)
+            return count++;
+      }
+  };
